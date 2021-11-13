cc28e1b2358a7bb2ee416826a28795d10f53aa58
animazaman <47300666+animazaman@users.noreply.github.com>
2021-11-14 00:19:09 +0600

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul>
+				<li><a class="facebook" href="https://www.facebook.com/"><span>Facebook</span></a></li>
+				<li><a class="twitter" href="https://www.twitter.com/"><span>Twitter</span></a></li>
+                <li><a class="googleplus" href="https://www.google.com/"><span>Google</span></a></li>
+				
+			</ul>
+		</div>
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2019 Tour and Travel Management System</p>
+	</div>
+</div>
